5c5468cfada5c1521bd143d58db168f6250e249f
KomiksPL <komikspl07@gmail.com>
2023-04-11 12:49:19 +0200

    Fixed and added a lot of things.

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,4 +1,8 @@
-﻿using MelonLoader;
+﻿using Il2Cpp;
+using Il2CppMonomiPark.SlimeRancher.SceneManagement;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [assembly: MelonInfo(typeof(MRF.EntryPoint), "MRF", "1.0.0", "Mod Contributors")]
 namespace MRF
@@ -6,5 +10,15 @@ namespace MRF
     public class EntryPoint : MelonMod
     {
         public override void OnInitializeMelon() { }
+        private static void Spawn(string name)
+        {
+            var instancePlayer = SRSingleton<SceneContext>.Instance.Player;
+            SRBehaviour.InstantiateActor(
+                Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(x => x.name.Equals(name)),
+                SRSingleton<SceneContext>.Instance.RegionRegistry.CurrentSceneGroup,
+                instancePlayer.transform.position, instancePlayer.transform.rotation);
+        }
+
+        public static void SpawnGastroPodForTest() => Spawn("brineGastropod");
     }
 }
--- HarmonyPatches.cs
+++ HarmonyPatches.cs
@@ -1,47 +1,107 @@
-﻿using HarmonyLib;
+﻿using System.Reflection;
+using HarmonyLib;
 using Il2Cpp;
+using Il2CppInterop.Runtime;
 using Il2CppMonomiPark.SlimeRancher;
 using Il2CppMonomiPark.SlimeRancher.DataModel;
 using Il2CppMonomiPark.SlimeRancher.Persist;
 using MelonLoader;
-using UnityEngine.Yoga;
+using UnityEngine;
+
+namespace MRF;
 
 internal class HarmonyPatches
 {
-    [HarmonyPatch(typeof(YogaConstants), nameof(YogaConstants.IsUndefined), typeof(float))]
-    public static class YogaConstantsIsUndefinedPatch
+    
+    
+    
+    
+    [HarmonyPatch]
+    internal static class Il2cppDetourMethodPatcherReportExceptionPatch
     {
-
-        public static bool Prefix(float value, ref bool __result)
+        public static MethodInfo TargetMethod() => AccessTools.Method(((IEnumerable<Type>) AccessTools.AllAssemblies().FirstOrDefault<Assembly>((Func<Assembly, bool>) (x => x.GetName().Name.Equals("Il2CppInterop.HarmonySupport"))).GetTypes()).FirstOrDefault<Type>((Func<Type, bool>) (x => x.Name == "Il2CppDetourMethodPatcher")), "ReportException");
+        public static bool Prefix(System.Exception ex)
         {
-            if (SavedGamePushActorDataPatch.pushActor)
-            {
-                SavedGamePushActorDataPatch.pushActor = false;
-                __result = true;
-                return false;
-            }
-            return true;
+            MelonLogger.Error("During invoking native->managed trampoline", ex);
+            return false;
         }
     }
 
-    [HarmonyPatch(typeof(SavedGame), nameof(SavedGame.PushActorData))]
-    public static class SavedGamePushActorDataPatch
+    [HarmonyPatch(typeof(PediaDirector), "Awake")]
+    public static class PediaDirectorAwake
     {
-        public static bool pushActor = false;
-        public static void Prefix(SavedGame __instance, GameModel gameModel, ActorDataV01 actorData, WorldV03 world)
+        public static bool IsTesting = false;
+        public static void Prefix(PediaDirector __instance)
         {
-
+            if (!IsTesting) return;
+            var pediaEntryCategory = __instance.entryCategories.items.ToArray().First(x => x.name == "Slimes");
+            var pediaEntry = pediaEntryCategory.items.ToArray().First();
+            var identifiablePediaEntry = ScriptableObject.CreateInstance<IdentifiablePediaEntry>();
+            var gastropodTest = Resources.FindObjectsOfTypeAll<IdentifiableType>().FirstOrDefault(x => x.name == "BrineGastropod");
+            identifiablePediaEntry.hideFlags |= HideFlags.HideAndDontSave;
+            identifiablePediaEntry.name = "Mud";
+            identifiablePediaEntry.identifiableType = gastropodTest;
+            identifiablePediaEntry.template = pediaEntry.template;
+            identifiablePediaEntry.title = gastropodTest.LocalizedName;
+            identifiablePediaEntry.description = gastropodTest.LocalizedName;
+            identifiablePediaEntry.isUnlockedInitially = false;
+            identifiablePediaEntry.actionButtonLabel = pediaEntry.actionButtonLabel;
+            identifiablePediaEntry.infoButtonLabel = pediaEntry.infoButtonLabel;
+            
+            pediaEntryCategory.items.Add(identifiablePediaEntry);
+            __instance.identDict.Add(gastropodTest, identifiablePediaEntry);
+        }
+    }
+    
+    
+    [HarmonyLib.HarmonyPatch(typeof(SavedGame))]
+    internal static class SavedGamePushPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(SavedGame.Push), typeof(GameModel))]
+        public static void PushGameModel(SavedGame __instance)
+        {
             IdentifiableTypePersistenceIdReverseLookupTable identifiableTypePersistenceIdReverseLookupTable = __instance.persistenceIdToIdentifiableType;
-            int typeId = actorData.typeId;
-            IdentifiableType identifiableType = identifiableTypePersistenceIdReverseLookupTable.GetIdentifiableType(typeId);
-            if (identifiableType == null)
+
+            var actorDataV01s = __instance.gameState.actors.ToArray().Where(x => identifiableTypePersistenceIdReverseLookupTable.GetIdentifiableType(x.typeId) is null);
+            foreach (var actorDataV01 in actorDataV01s)
             {
-                pushActor = true;
-                MelonLogger.Msg("identType is null");
+                MelonLogger.Warning($"Actor is null, removing actor: {actorDataV01.typeId}");
+                __instance.gameState.actors.Remove(actorDataV01);
+            }
+            var pediaStrings = __instance.gameState.pedia.unlockedIds.ToArray().Where(pediaUnlockedId => !__instance.pediaEntryLookup.ContainsKey(pediaUnlockedId));
+            foreach (var pediaString in pediaStrings)
+            {
+                MelonLogger.Warning($"PediaId is null, removing pedia named {pediaString}");
+                __instance.gameState.pedia.unlockedIds.Remove(pediaString);
 
             }
 
-            //MelonLogger.Msg(identifiableType.ToString());
+            List<LandPlotV02> landPlotV02s = new List<LandPlotV02>();
+            foreach (var landPlotV02 in __instance.gameState.ranch.plots)
+            {
+                if (!Enum.IsDefined(landPlotV02.typeId))
+                {
+                    landPlotV02s.Add(landPlotV02);
+                    continue;
+                }
+                var upgrades = new List<LandPlot.Upgrade>();
+                foreach (var upgrade in landPlotV02.upgrades)
+                {
+                    if (!Enum.IsDefined(upgrade))
+                    {
+                        upgrades.Add(upgrade);
+                    }
+                }
+                upgrades.ForEach(x => landPlotV02.upgrades.Remove(x) );
+            }
+            //var landPlotV02s = __instance.gameState.ranch.plots.ToArray().Where(x => !Enum.IsDefined(x.typeId));
+            foreach (var landPlotV02 in landPlotV02s)
+            {
+                MelonLogger.Warning($"Plot TypeId is NONE, removing plot: {landPlotV02.typeId}");
+                __instance.gameState.ranch.plots.Remove(landPlotV02);
+            }
+
         }
     }
 }
