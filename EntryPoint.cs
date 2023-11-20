bb03bac7ef343d0d7d53bef6adef92766912af4f
ItzBlueBerries <fruitsyberries@gmail.com>
2023-11-19 19:45:51 -0500

    Changed version number

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -4,7 +4,7 @@ using MelonLoader;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[assembly: MelonInfo(typeof(MRF.EntryPoint), "MRF", "1.0.0", "Mod Contributors")]
+[assembly: MelonInfo(typeof(MRF.EntryPoint), "MRF", "1.0.2", "Mod Contributors")]
 namespace MRF
 {
     public class EntryPoint : MelonMod
